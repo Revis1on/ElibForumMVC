7926ce0a85280cfe7e04785b3b57638d99fbdcbf
Revis1on <38330826+Revis1on@users.noreply.github.com>
2020-09-20 00:00:20 +0200

    Add project files.

---
+++ ElibForumMVC.Data/IForum.cs
@@ -0,0 +1,21 @@
+﻿using ElibForumMVC.Data.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ElibForumMVC.Data
+{
+    public interface IForum
+    {
+
+        Forum GetById(int id);
+        IEnumerable<Forum> GetAll();
+        IEnumerable<ApplicationUser> GetAllActiveUsers();
+
+        Task Create(Forum forum);
+        Task Delete(int forumID);
+        Task UpdateForumTitile(int forumId, string newTitle);
+        Task DeleteForumDescription(int forumId, string newDescription);
+
+
+    }
+}
---
+++ ElibForumMVC.Data/IPost.cs
@@ -0,0 +1,23 @@
+﻿using ElibForumMVC.Data.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ElibForumMVC.Data
+{
+    public interface IPost
+    {
+        Post getById(int id);
+        IEnumerable<Post> getAll();
+        IEnumerable<Post> getFilteredPosts(Forum forum, string searchQuery);
+        IEnumerable<Post> getFilteredPosts(string searchQuery);
+        IEnumerable<Post> getPostByForum(int id);
+        IEnumerable<Post> GetLatestPosts(int n);
+
+        Task Add(Post post);
+        Task Delete(int id);
+        Task EditPostContent(int id, string newContent);
+
+        Task AddReply(PostReply reply);
+
+    }
+}
